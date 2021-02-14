a910f5c0901bd8e6812e86be7a3d60d7b03595d9
Vijaya Kaushik <VijayaKaushik2019@u.northwestern.edu>
2021-02-13 19:00:22 -0500

    Commit Message Initial

---
+++ Hello.py
@@ -0,0 +1,33 @@
+import os
+os.system('clear')
+
+# Print Hello World! on the screen
+
+print ("Hello World!")
+
+##################################
+## Data Types
+# Strings
+Name ="vijaya"
+# Numbers
+Age= 40
+#lists - starts with zero
+family =["Priyank","Nicky","Cinni"]
+print (family[1:3])
+#Tuples - Cannot chage once defined. it's faster thann list
+family_t=("priyank","Nicky","cinni")
+
+print(family_t[1])
+# Dictonaries ; Key - value pair 
+
+fam_age = {"Priyank" : 39,
+			"Nicky" : 36,
+			"Cinni" :32
+	
+			}
+print(fam_age["Cinni"])
+# Boolean
+
+name_t = True
+
+###################################
